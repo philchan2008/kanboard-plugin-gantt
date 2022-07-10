2a02fa59d544d9f2d2b6160e1dd9b2aeefcff08e
phil <phil@flexsystem.com>
2022-07-10 12:04:20 +0800

    * add scrollTop by drag-n-drop within ganttview area

--- Assets/gantt.js
+++ Assets/gantt.js
@@ -26,15 +26,20 @@ $(function() {
     var isDragging = false;
     var offsetX = 0;
     var offsetLeft = 0;
+    var offsetY = 0;
+    var offsetTop = 0;
     $("div.ganttview-slide-container")
     .mousedown(function(e) {
         isDragging = true;
         offsetX = e.pageX; 
         offsetLeft = $("div.ganttview-slide-container").scrollLeft();
+        offsetY = e.pageY;
+        offsetTop = $(document).scrollTop();
     })
     .mousemove(function(e) {
         if (isDragging) {
             $("div.ganttview-slide-container").scrollLeft(offsetLeft + (e.pageX - offsetX)*-1);
+            $(document).scrollTop(offsetTop + (e.pageY - offsetY)*-1);
         }
      })
     .mouseup(function(e) {
