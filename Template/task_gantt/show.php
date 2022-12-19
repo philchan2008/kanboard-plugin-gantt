91941aa2fa964bd48b6cf2096469f8cba2bc6363
phil <phil@flexsystem.com>
2022-12-19 11:48:19 +0800

    * move task slide control out of the chart * update margin-bottom to make center of the slide

--- Template/task_gantt/show.php
+++ Template/task_gantt/show.php
@@ -2,37 +2,44 @@
     <?= $this->projectHeader->render($project, 'TaskGanttController', 'show', false, 'Gantt') ?>
     <div class="menu-inline">
         <ul class="views">
-            <li <?= $sorting === 'id' ? 'class="active"' : '' ?>>
-                <?= $this->url->icon('sort-numeric-asc', t('Sort by ID'), 'TaskGanttController', 'show', array('project_id' => $project['id'], 'sorting' => 'id', 'plugin' => 'Gantt')) ?>
-            </li>
+            <li>
+                <span  >
+                    <i class="fa fa-tasks" >
+                        <input type="range" class="grant-view-slide-controls" id="ganttTaskTitleWidth" name="ganttTaskTitleWidth"  min="50" max="1500" value="500" />
+                    </i>
+                </span>
+            </li>  
             <li <?= $sorting === 'board' ? 'class="active"' : '' ?>>
                 <?= $this->url->icon('sort-numeric-asc', t('Sort by position'), 'TaskGanttController', 'show', array('project_id' => $project['id'], 'sorting' => 'board', 'plugin' => 'Gantt')) ?>
             </li>
-            <li <?= $sorting === 'name' ? 'class="active"' : '' ?>>
-                <?= $this->url->icon('sort-numeric-asc', t('Sort by name'), 'TaskGanttController', 'show', array('project_id' => $project['id'], 'sorting' => 'name', 'plugin' => 'Gantt')) ?>
-            </li>
             <li <?= $sorting === 'date' ? 'class="active"' : '' ?>>
                 <?= $this->url->icon('sort-amount-asc', t('Sort by date'), 'TaskGanttController', 'show', array('project_id' => $project['id'], 'sorting' => 'date', 'plugin' => 'Gantt')) ?>
+            </li>
+            <li <?= $sorting === 'id' ? 'class="active"' : '' ?>>
+                <?= $this->url->icon('sort-numeric-asc', t('Sort by ID'), 'TaskGanttController', 'show', array('project_id' => $project['id'], 'sorting' => 'id', 'plugin' => 'Gantt')) ?>
+            </li>
+            <li <?= $sorting === 'name' ? 'class="active"' : '' ?>>
+                <?= $this->url->icon('sort-numeric-asc', t('Sort by name'), 'TaskGanttController', 'show', array('project_id' => $project['id'], 'sorting' => 'name', 'plugin' => 'Gantt')) ?>
             </li>
             <li>
                 <?= $this->modal->large('plus', t('Add task'), 'TaskCreationController', 'show', array('project_id' => $project['id'])) ?>
             </li>
             <li>
-            <span  >
-                <i class="fa fa-th"></i>
-                <i class="fa fa-arrows-h" >
-                <input type="range" id="zoomFactorW" name="zoomFactorW"  min="850" max="1050" />
-                </i>
-                <i class="fa fa-arrows-v" >
-                <input type="range" id="zoomFactorH" name="zoomFactorH"  min="750" max="1250" />
-                </i>
-                <i class="fa fa-th-large"></i>
-            </span>  
-            </li>
-            <!-- <li>
-            <a href="" class="view-refresh"><i class="fa fa-refresh" aria-hidden="true"></i>Refresh</a>
+                <span  >
+                    <i class="fa fa-th"></i>
+                    <i class="fa fa-arrows-h" >
+                        <input type="range" class="grant-view-slide-controls" id="zoomFactorW" name="zoomFactorW"  min="850" max="1050" />
+                    </i>
+                    <i class="fa fa-arrows-v" >
+                        <input type="range" class="grant-view-slide-controls" id="zoomFactorH" name="zoomFactorH"  min="750" max="1250" />
+                    </i>
+                    <i class="fa fa-th-large"></i>
+                </span>  
             </li>
             <li>
+            <a href="" class="view-refresh"><i class="fa fa-refresh" aria-hidden="true"></i>Refresh</a>
+            </li>
+            <!-- <li>
             <a href="javascript:" id="button-print"><i class="fa fa-print" aria-hidden="true"></i>Print</a>
             </li> -->
         </ul>        
